c326570ae3095355a8adfe12b2a523b04b5d3e39
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-27 11:50:18 +0530

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -23,26 +23,82 @@ interface ImageData {
   }>;
 }
 
+// export default async function Home() {
+//   const allImages = await fetch(
+//     `https://api.tryleap.ai/api/v1/images/models/26a1a203-3a46-42cb-8cfa-f4de075907d8/inferences?onlyFinished=true&pageSize=20`,
+//     {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//         authorization: "Bearer bd130c31-55ab-46a6-97f9-ca7843aae065",
+//       },
+//       cache: "no-cache",
+//     }
+//   );
+
+//   const data = await allImages.json();
+//   console.log(data);
+//   const finishedImages = data.filter(
+//     (image: ImageData) => image.state === "finished"
+//   );
+
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-center p-10 md:p-24">
+//       <div className="grid grid-cols-1 2xl:grid-cols-2">
+//         {finishedImages
+//           .slice()
+//           .reverse()
+//           .map((image: ImageData, index: number) => (
+//             <Card
+//               key={index}
+//               className="p-5 w-[500px] h-[380px] overflow-hidden border-hidden"
+//             >
+//               <CardTitle className=" line-clamp-2">{image.prompt}</CardTitle>
+//               <CardContent className="mt-3 relative max-w-xl h-full">
+//                 <Image
+//                   src={image.images[0].uri}
+//                   alt={image.prompt}
+//                   fill
+//                   className="rounded-lg"
+//                   loading="lazy"
+//                 />
+//               </CardContent>
+//             </Card>
+//           ))}
+//       </div>
+//     </main>
+//   );
+// }
+
 export default async function Home() {
   const allImages = await fetch(
-    `https://api.tryleap.ai/api/v1/images/models/26a1a203-3a46-42cb-8cfa-f4de075907d8/inferences?onlyFinished=false`,
+    `https://api.tryleap.ai/api/v1/images/models/26a1a203-3a46-42cb-8cfa-f4de075907d8/inferences?onlyFinished=true&pageSize=20`,
     {
       method: "GET",
       headers: {
         Accept: "application/json",
         authorization: "Bearer bd130c31-55ab-46a6-97f9-ca7843aae065",
       },
+      cache: "no-cache",
     }
   );
+
   const data = await allImages.json();
-  const finishedImages = data.filter(
-    (image: ImageData) => image.state === "finished"
-  );
+  console.log(data);
+  
+  const prompts = new Set();
+  const uniquePromptImages = data.filter((image: ImageData) => {
+    if (image.state === "finished" && !prompts.has(image.prompt)) {
+      prompts.add(image.prompt);
+      return true;
+    }
+    return false;
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-10 md:p-24">
       <div className="grid grid-cols-1 2xl:grid-cols-2">
-        {finishedImages
+        {uniquePromptImages
           .slice()
           .reverse()
           .map((image: ImageData, index: number) => (
